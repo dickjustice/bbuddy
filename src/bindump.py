919084d58a822daab401c249687db75fa694b8bd
Dick Justice <dickjustice@x2400.com>
2020-12-07 09:03:10 -0600

    minor improvements to bindump

--- src/bindump.py
+++ src/bindump.py
@@ -8,10 +8,16 @@
 import sys
 import os
 
+def exit_fail( msg ):
+    print( "FAIL: %s" % msg )
+    exit()
+
 def usage_bail():
     print( "usage: bindump [opts] fn [opts]")
     print( "Possible opts:" )
-    print( "  -a : dump entire file. Otherwise dumps first 256 bytes only" )
+    print( "  -nb <bytes> : Dumps up to a number of bytes. 0 means full file." )
+    print( "  -a : dump entire file.  Equivalient to '-nb 0' ") 
+    print( "If no options are specified, prints first 256 bytes")
     exit()
 
 #  if nb<=0, it means to print all of it
@@ -63,18 +69,29 @@ def dumpmem( contents, descr='data', nb=0 ):
     return
 
 def main( argv ):
+    mode = 'mode_none'
     fn = None
     nb = 256
 
     for arg in argv[1:]:
-        if arg=='-a':
-            nb=0
-        elif os.path.isfile( arg ):
-            fn=arg
+        if mode=='mode_none':
+            if arg=='-a':
+                nb=0
+            elif arg=='-nb':
+                mode='mode_nb' 
+            elif os.path.isfile( arg ):
+                fn=arg
+            else:
+                print( "'%s' is neither an option nor a filename" % arg )
+                usage_bail()
+        elif mode=='mode_nb':
+            try:
+                nb = int( arg, 0 )
+            except:
+                exit_fail( "'%s' not convertable to int" )
+            mode='mode_none'
         else:
-            print( "'%s' is neither an option nor a filename" % arg )
-            usage_bail()
-
+            exit_fail( "illegal mdoe '%s'" % mode )
     if fn is None:
         print( "No file specified to dump" )
         usage_bail()            

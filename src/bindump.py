09e6b4468aa8ace341c04146fa9d16f364ad6615
Dick Justice <dickjustice@x2400.com>
2020-12-11 16:47:20 -0600

    bindump

--- src/bindump.py
+++ src/bindump.py
@@ -7,99 +7,125 @@
 
 import sys
 import os
+import struct
 
-def exit_fail( msg ):
-    print( "FAIL: %s" % msg )
-    exit()
+RED="\033[1;31m"; YELLOW="\033[0;33m"; GREEN="\033[0;32m"; RESET="\033[0;0m"; BOLD="\033[;1m";BLUE="\033[1;34m";MAGENTA="\033[1;35m"
 
-def usage_bail():
-    print( "usage: bindump [opts] fn [opts]")
-    print( "Possible opts:" )
-    print( "  -nb <bytes> : Dumps up to a number of bytes. 0 means full file." )
-    print( "  -a : dump entire file.  Equivalient to '-nb 0' ") 
-    print( "If no options are specified, prints first 256 bytes")
-    exit()
+def dump_line_of_ints( ix0,memm, formatt ):
+    print( "%3x: " % ix0, end='' )
+    done=False
+    for i in range(4):
+        data=memm[4*i:4*i+4]
+        if len(data)==4:
+            v = struct.unpack( formatt, data )
+            print( '%08x ' % v, end='' )
+        elif len(data)>0:
+            ss = ('??'*len(data)).ljust(9)
+            print( ss,end='')
+        else:
+            print( ' '*9, end='' )
+    print( '' )
+
+def le_32s( ix0, memm ):
+    dump_line_of_ints( ix0, memm, '<I' )
+
+def be_32s( ix0, memm ):
+    dump_line_of_ints( ix0, memm, '>I' )
+
+#  dump up to 16 bytes of data as both hex and ascii
+def dump_ascii_line( ix0, memm ):
+    nb=len(memm)
+    print( "%3x: " % ix0, end='' )
+    for i in range(nb):
+        bval = memm[i]
+        print( "%02x " % bval, end='' )
+    for i in range(nb,16):
+        print( "   ", end='' )
+    print( "| ", end='')
+    for i in range(nb):
+        bval = memm[i]
+        if bval >=0x20 and bval <= 0x7e:
+            bstr = memm[i: i+1]
+            strr = bstr.decode( 'utf-8')
+            print( strr, end='')
+        else:
+            print('.', end='')
+    for i in range(nb,16):
+        print( " ", end='')
+
+    print( '')
+    return()
 
 #  if nb<=0, it means to print all of it
-def dumpmem( contents, descr='data', nb=0 ):
+def dumpmem( contents, descr='data', nb=0, formatt=dump_ascii_line ):
     mode_all= True if nb<=0 else False
 
     if mode_all:
-        print( "all %s:" % descr )
+        print( "All %s:" % descr )
         nb_to_print = len(contents)
     else:
         if len(contents) <= nb:
             print( "all %s:" % descr )
-            #print( "contents of entire file '%s':" % fn )
             nb_to_print = len(contents)
         else:
             print( "beginning of %s:" % descr )
-            #print( "beginning of file '%s':" % fn )
             nb_to_print = nb
+    print( "chars to print:", nb_to_print)
+    data_to_print = contents[ :nb_to_print]
 
-    ix0 = 0;
     done=False
+    ix0=0
     while not done:
-        print( "%3x: " % ix0, end='' )
-        for i in range(16):
-            ixx = ix0+i
-            if ixx<nb_to_print:
-                bval = contents[ixx]
-                print( "%02x " % bval, end='' )
-            else:
-                print( "   ", end='' )
-        print( "| ", end='')
-
-        for i in range(16):
-            ixx = ix0+i
-            if ixx<nb_to_print:
-                bval = contents[ixx]
-                if bval >=0x20 and bval <= 0x7e:
-                    bvalbinstr = contents[ixx: ixx+1]
-                    str = bvalbinstr.decode( 'utf-8')
-                    print( str, end='')
-                else:
-                    print( ".", end='')
-            else:
-                print( " ", end='')
-        print( '')
-        ix0+=16
-        if ix0>= nb_to_print:
+        printline = data_to_print[:16]
+        if len(printline)>0 :
+            formatt( ix0,  printline )
+            #dump_ascii_line( ix0,  printline )
+        else:
             done=True
+        ix0+=16
+        data_to_print = data_to_print[16:] 
+
     return
 
+
+def usage_bail():
+    print( "usage: bindump [opts] fn [opts]")
+    print( "Possible opts:" )
+    print( "  -a  : dump entire file. Otherwise dumps first 256 bytes only" )
+    print( "  -le : dump data as array of 32-bit LE ints" )
+    print( "  -be : dump data as array of 32-bit BE ints" )
+    exit()
+
+
 def main( argv ):
-    mode = 'mode_none'
     fn = None
     nb = 256
+    formatt=dump_ascii_line 
 
     for arg in argv[1:]:
-        if mode=='mode_none':
-            if arg=='-a':
-                nb=0
-            elif arg=='-nb':
-                mode='mode_nb' 
-            elif os.path.isfile( arg ):
-                fn=arg
-            else:
-                print( "'%s' is neither an option nor a filename" % arg )
-                usage_bail()
-        elif mode=='mode_nb':
-            try:
-                nb = int( arg, 0 )
-            except:
-                exit_fail( "'%s' not convertable to int" )
-            mode='mode_none'
+        if arg=='-a':
+            nb=0
+        elif arg=='-le':
+            formatt=le_32s
+            pass
+        elif arg=='-be':
+            formatt=be_32s
+            pass
+
+        elif os.path.isfile( arg ):
+            fn=arg
         else:
-            exit_fail( "illegal mdoe '%s'" % mode )
+            print( "'%s' is neither an option nor a filename" % arg )
+            usage_bail()
+
     if fn is None:
         print( "No file specified to dump" )
         usage_bail()            
 
     with open( fn, 'rb' ) as f:
-        contents = f.read()
+        bindata = f.read()
 
-    dumpmem( contents, descr="file '%s'"%fn, nb=nb )
+    dumpmem( bindata, descr="file '%s'"%fn, nb=nb, formatt=formatt )
     return
 
 #---------

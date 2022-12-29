d80fd7b58099ac08a3abd32bc967ee211b943d01
Dick Justice <dickjustice@x2400.com>
2022-12-29 11:33:18 -0500

    style and lint cleanup

--- src/fffind.py
+++ src/fffind.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
-#
-#   fffind.py
-#
-#  (c) 2020 Dick Justice
-#  Released under MIT license.
+"""
+fffind.py
+(c) 2020-2022 Dick Justice
+Released under MIT license.
+"""
 
-import sys,os,subprocess
+import sys
+import os
 from pathlib import Path
 
-FFFIND_VER = '0.0.003'
+FFFIND_VER = '0.0.004'
 
-
-RED="\033[1;31m"; YELLOW="\033[0;33m"; GREEN="\033[0;32m"; RESET="\033[0;0m"; BOLD="\033[;1m";BLUE="\033[1;34m"
+RED="\033[1;31m"
+YELLOW="\033[0;33m"
+GREEN="\033[0;32m"
+RESET="\033[0;0m"
+BOLD="\033[;1m"
+BLUE="\033[1;34m"
 BOLD_YELLOW  = "\033[1;33m"
 BOLD_RED=  "\033[1;31m"
 
@@ -19,136 +24,137 @@ BOLD_RED=  "\033[1;31m"
 #  match is a boolean, corresponding to whether it matches
 #  colorized_file is the file name with colorization added
 def does_it_match( file, what ):
-   colorized_file=''
+    colorized_file=''
 
-   if what=='*':
-      matches=True
-      colorized_file = file   #for '*' don't colorize at all
+    if what=='*':
+        matches=True
+        colorized_file = file   #for '*' don't colorize at all
 
-   elif what.startswith( '*' ) and what.endswith( '*' ):
-      mval = what[1:-1]
-      matches = mval in file 
-      if matches:
-         parts = file.split( mval )
-         jval = BOLD_RED + mval + RESET
-         colorized_file = jval.join( parts )
+    elif what.startswith( '*' ) and what.endswith( '*' ):
+        mval = what[1:-1]
+        matches = mval in file
+        if matches:
+            parts = file.split( mval )
+            jval = BOLD_RED + mval + RESET
+            colorized_file = jval.join( parts )
 
-   elif what.startswith( '*' ):        # Example:  *.py 
-      mval = what[1:]
-      matches = file.endswith( mval ) 
-      if matches:
-         colorized_file = file[:len(file)-len(mval)] + BOLD_RED + mval + RESET
+    elif what.startswith( '*' ):        # Example:  *.py
+        mval = what[1:]
+        matches = file.endswith( mval )
+        if matches:
+            colorized_file = file[:len(file)-len(mval)] + BOLD_RED + mval + RESET
 
-   elif what.endswith( '*' ):          # example: sammy*
-      mval = what[:-1]
-      matches = file.startswith( mval ) 
-      if matches:
-         colorized_file = BOLD_RED + mval + RESET + file[len(mval):]
-   else:
-      # exact match required
-      mval = what
-      matches = file==mval
-      if matches:
-         colorized_file = file
-   return( matches,  colorized_file )
+    elif what.endswith( '*' ):          # example: sammy*
+        mval = what[:-1]
+        matches = file.startswith( mval )
+        if matches:
+            colorized_file = BOLD_RED + mval + RESET + file[len(mval):]
+    else:
+        # exact match required
+        mval = what
+        matches = file==mval
+        if matches:
+            colorized_file = file
+    return( matches,  colorized_file )
 
 
 def does_it_match_and_please_colorize( file, root, what, colorize=True ):
-   matches, colorized_file = does_it_match( file, what )
-   fn_w_path =  root + '/' + file 
-   if matches and colorize:
-      fn_w_path =  root + '/' + colorized_file 
-   return matches, fn_w_path 
+    matches, colorized_file = does_it_match( file, what )
+    fn_w_path =  root + '/' + file
+    if matches and colorize:
+        fn_w_path =  root + '/' + colorized_file
+    return matches, fn_w_path
 
 def directory_is_excluded( root, d, exclude_dirs ):
-   for exx in exclude_dirs:
-      if '/' in exx:
-         if os.path.abspath(root+'/'+d) == os.path.abspath(exx):
-            return True
-      else:
-         if d==exx:
-            return True
-   return False
+    for exx in exclude_dirs:
+        if '/' in exx:
+            if os.path.abspath(root+'/'+d) == os.path.abspath(exx):
+                return True
+        else:
+            if d==exx:
+                return True
+    return False
 
 def usage_bail():
-   print( "fffind v %s" % FFFIND_VER )
-   print( "Usage: fffind [-f|-d] [-bw] [-x dir] <what>")
-   print( "  <what> may be like:" ) 
-   print( "     abc     : is exactly abc" ) 
-   print( "     'abc*'  : starts with abc" ) 
-   print( "     '*abc'  : ends with abc" ) 
-   print( "     '*abc*' : contains abc" ) 
-   print( "     '*'     : all files and directories match" ) 
-   print( "  options:" ) 
-   print( "     -f     : files only" )
-   print( "     -d     : directories only")
-   print( "     -bw    : black and white output")
-   print( "     -x dir : exclude this directory. may be used multpiple times")
-   print( "              if dir contains '/', only the one instance is excluded" )
-   print( "              otherwise, any dir with that name found is excluded" )
-   print( "If file '.exclude' is present at any level, it excludes its listed directories")
-   exit()
+    print( f"{sys.argv[0]} v {FFFIND_VER}"  )
+    print( "Usage: fffind [-f|-d] [-bw] [-x dir] <what>")
+    print( "  <what> may be like:" )
+    print( "     abc     : is exactly abc" )
+    print( "     'abc*'  : starts with abc" )
+    print( "     '*abc'  : ends with abc" )
+    print( "     '*abc*' : contains abc" )
+    print( "     '*'     : all files and directories match" )
+    print( "  options:" )
+    print( "     -f     : files only" )
+    print( "     -d     : directories only")
+    print( "     -bw    : black and white output")
+    print( "     -x dir : exclude this directory. may be used multpiple times")
+    print( "              if dir contains '/', only the one instance is excluded" )
+    print( "              otherwise, any dir with that name found is excluded" )
+    print( "If file '.exclude' is present at any level, it excludes its listed directories")
+    sys.exit(1)
 
 def main( argv ):
-   exclude_dirs = []
-   report_files=True
-   report_dirs= True
-   colorize=True
+    exclude_dirs = []
+    report_files=True
+    report_dirs= True
+    colorize=True
 
-   if len(argv)<2:
-      usage_bail()
+    if len(argv)<2:
+        usage_bail()
 
-   what = argv[-1]
-   opts = argv[ 1:-1 ]
-   exclude_opt_next=False
-   for opt in opts:
-      if exclude_opt_next:
-         exclude_dirs.append( opt )
-         exclude_opt_next=False
-      else:
-         if opt=='-d':
-            report_files=False
-         elif opt=='-f':
-            report_dirs=False
-         elif opt=='-x':
-            exclude_opt_next=True
-         elif opt=='-bw':
-            colorize=False
-         else:
-            print( "invalid option '%s'" % opt )
-            usage_bail()
+    what = argv[-1]
+    opts = argv[ 1:-1 ]
+    exclude_opt_next=False
+    for opt in opts:
+        if exclude_opt_next:
+            exclude_dirs.append( opt )
+            exclude_opt_next=False
+        else:
+            if opt=='-d':
+                report_files=False
+            elif opt=='-f':
+                report_dirs=False
+            elif opt=='-x':
+                exclude_opt_next=True
+            elif opt=='-bw':
+                colorize=False
+            else:
+                print( f"invalid option '{opt}'"  )
+                usage_bail()
 
-   for root, dirs, files in os.walk('.'):
-      if '.exclude' in files:
-         with open(root+'/.exclude') as f:
-            lines = f.readlines()
-         for line in lines:
-            strippedline = line.strip()
-            if len(strippedline)>0 and not strippedline.startswith( '#' ):
-               exclude_dirs.append( root +'/'+strippedline  )
+    for root, dirs, files in os.walk('.'):
+        if '.exclude' in files:
+            with open(root+'/.exclude', encoding='utf-8') as f:
+                lines = f.readlines()
+            for line in lines:
+                strippedline = line.strip()
+                if len(strippedline)>0 and not strippedline.startswith( '#' ):
+                    exclude_dirs.append( root +'/'+strippedline  )
 
-      newdirs=[]
-      for d in dirs:
-         if not directory_is_excluded( root, d, exclude_dirs):
-            #newdirs.append(Path(d).as_posix()) 
-            newdirs.append(d)
-      dirs[:] = newdirs
+        newdirs=[]
+        for d in dirs:
+            if not directory_is_excluded( root, d, exclude_dirs):
+                newdirs.append(d)
+        dirs[:] = newdirs
 
-      root = Path(root).as_posix()
-      if report_files:
-         for file in files:
-            matches, fn_czd = does_it_match_and_please_colorize( file, root, what, colorize=colorize )
-            if matches:
-               print( fn_czd )
-      if report_dirs:
-         for dirr in dirs:
-            matches, dir_czd = does_it_match_and_please_colorize( dirr, root, what, colorize=colorize )
-            if matches:
-               print( dir_czd + '/' )
+        root = Path(root).as_posix()
+        if report_files:
+            for file in files:
+                matches, fn_czd = does_it_match_and_please_colorize(
+                                    file, root, what, colorize=colorize )
+                if matches:
+                    print( fn_czd )
+        if report_dirs:
+            for dirr in dirs:
+                matches, dir_czd = does_it_match_and_please_colorize(
+                                    dirr, root, what, colorize=colorize )
+                if matches:
+                    print( dir_czd + '/' )
 
-   if len(exclude_dirs)>0:
-      xdirs = [  Path(x).as_posix()+'/' for x in exclude_dirs ]   
-      print( "Note: These dirs excluded from search:",', '.join(xdirs) )
+    if len(exclude_dirs)>0:
+        xdirs = [  Path(x).as_posix()+'/' for x in exclude_dirs ]
+        print( "Note: These dirs excluded from search:",', '.join(xdirs) )
 
 #---------
 if __name__ == "__main__":
--- src/ppgrep.py
+++ src/ppgrep.py
@@ -1,127 +1,131 @@
 #!/usr/bin/env python3
-#
-#  ppgrep.py
-#
-#  (c) 2020 Dick Justice
-#  Released under MIT license.
+'''
+ppgrep.py
+(c) 2020-2022 Dick Justice
+Released under MIT license.
+'''
 
-import sys,os,time,subprocess
-from datetime import datetime
+import sys
+import os
 from pathlib import Path
 
-PPGREP_VER='0.0.005'
+PPGREP_VER='0.0.006'
 
-RED="\033[1;31m"; YELLOW="\033[0;33m"; GREEN="\033[0;32m"; RESET="\033[0;0m"; BOLD="\033[;1m";BLUE="\033[1;34m";MAGENTA="\033[1;35m"
+RED="\033[1;31m"
+YELLOW="\033[0;33m"
+GREEN="\033[0;32m"
+RESET="\033[0;0m"
+BOLD="\033[;1m"
+BLUE="\033[1;34m"
+MAGENTA="\033[1;35m"
 BOLD_YELLOW  = "\033[1;33m"
 BOLD_RED=  "\033[1;31m"
 
 #
 def colorize_it( file, what  ):
-   parts = file.split( what )
-   jval = BOLD_RED + what + RESET
-   rval = jval.join( parts )
-   return( rval )
+    parts = file.split( what )
+    jval = BOLD_RED + what + RESET
+    rval = jval.join( parts )
+    return rval
 
 def does_it_match( file, what ):
-   if what=='*':
-      mval = ''
-      matches = True 
-   elif what.startswith( '*' ) and what.endswith( '*' ):
-      mval = what[1:-1]
-      matches = mval in file 
+    if what=='*':
+        mval = ''
+        matches = True
+    elif what.startswith( '*' ) and what.endswith( '*' ):
+        mval = what[1:-1]
+        matches = mval in file
 
-   elif what.startswith( '*' ):        # Example:  *.py 
-      mval = what[1:]
-      matches = file.endswith( mval ) 
+    elif what.startswith( '*' ):        # Example:  *.py
+        mval = what[1:]
+        matches = file.endswith( mval )
 
-   elif what.endswith( '*' ):          # example: sammy*
-      mval = what[:-1]
-      matches = file.startswith( mval ) 
-   else:
-      # exact match required
-      mval = what
-      matches = file==mval
-   return( matches )
+    elif what.endswith( '*' ):          # example: sammy*
+        mval = what[:-1]
+        matches = file.startswith( mval )
+    else:
+        # exact match required
+        mval = what
+        matches = file==mval
+    return matches
 
 
 def directory_is_excluded( root, d, exclude_dirs ):
-   for exx in exclude_dirs:
-      if '/' in exx:
-         if os.path.abspath(root+'/'+d) == os.path.abspath(exx):
-            return True
-      else:
-         if d==exx:
-            return True
-   return False
+    for exx in exclude_dirs:
+        if '/' in exx:
+            if os.path.abspath(root+'/'+d) == os.path.abspath(exx):
+                return True
+        else:
+            if d==exx:
+                return True
+    return False
 
 
 def usage_bail():
-   print( "ppgrep v %s"% PPGREP_VER )
-   print( "Usage: ppgrep <what> [fn_match] [fn_match_2] .." )
-   print( "   Recursively searches for string <what> in files under local dir" )
-   print( "   If 'fn_match' not spcified, searches all files (uses '*') ")
-   print( "If file '.exclude' is present at any level, it excludes its listed directories")
-   exit()
+    print( f"{sys.argv[0]} v {PPGREP_VER}" )
+    print(  "Usage: ppgrep <what> [fn_match] [fn_match_2] .." )
+    print(  "   Recursively searches for string <what> in files under local dir" )
+    print(  "   If 'fn_match' not spcified, searches all files (uses '*') ")
+    print(  "If file '.exclude' is present at any level, it excludes its listed directories")
+    sys.exit(1)
 
 def main( argv ):
-   if len(argv)<2:
-      usage_bail()
+    if len(argv)<2:
+        usage_bail()
 
-   what = argv[1]
-   fn_match_list=[]
+    what = argv[1]
+    fn_match_list=[]
 
-   fn_match_args = argv[2:]
-   if len(fn_match_args)>0:
-      for arg in fn_match_args:
-         fn_match_list.append( arg )
-   else:
-      fn_match_list.append( '*' )
+    fn_match_args = argv[2:]
+    if len(fn_match_args)>0:
+        for arg in fn_match_args:
+            fn_match_list.append( arg )
+    else:
+        fn_match_list.append( '*' )
 
-   exclude_dirs=[]
-   searchfiles=[]
-   for root, dirs, files in os.walk('.'):
-      if '.exclude' in files:
-         with open(root+'/.exclude') as f:
-            lines = f.readlines()
-         for line in lines:
-            strippedline = line.strip()
-            if len(strippedline)>0 and not strippedline.startswith( '#' ):
-               exclude_dirs.append( root+'/'+strippedline  )
+    exclude_dirs=[]
+    searchfiles=[]
+    for root, dirs, files in os.walk('.'):
+        if '.exclude' in files:
+            with open(root+'/.exclude', encoding='utf-8') as f:
+                lines = f.readlines()
+            for line in lines:
+                strippedline = line.strip()
+                if len(strippedline)>0 and not strippedline.startswith( '#' ):
+                    exclude_dirs.append( root+'/'+strippedline  )
 
-      newdirs=[]
-      for d in dirs:
-         if not directory_is_excluded( root, d, exclude_dirs):
-            newdirs.append(d)
-      dirs[:] = newdirs
+        newdirs=[]
+        for d in dirs:
+            if not directory_is_excluded( root, d, exclude_dirs):
+                newdirs.append(d)
+        dirs[:] = newdirs
 
-      root_good = Path(root).as_posix()   #deal with windows paths
-
-      for file in files:
-         for where in fn_match_list:
-            if does_it_match( file, where ):
-               searchfiles.append( (Path(root).as_posix() +'/'+ file) )
+        for file in files:
+            for where in fn_match_list:
+                if does_it_match( file, where ):
+                    searchfiles.append( (Path(root).as_posix() +'/'+ file) )
 
 
-   for fn in searchfiles:
-      if os.path.exists(fn):
-         f=open(fn,'rb')
-         contents=f.read()
-         f.close()
-         lines_bin = contents.splitlines()
-         for ix,line_bin in enumerate( lines_bin ):
-            try:
-               line = line_bin.decode( 'utf-8')
-               if what in line:
-                  print( "%s (%d): %s" % (fn, ix+1,   colorize_it(line,what)  ))
-            except:
-               #most of the time, no desire to print here. zip, other binaries, etc cause issues
-               #print( "problem decoding in '%s'" % fn )
-               pass
-   if len(exclude_dirs)>0:
-      xdirs = [  Path(x).as_posix()+'/' for x in exclude_dirs ]   
-      print( "Note: These dirs excluded from search:",', '.join(xdirs) )
+    for fn in searchfiles:
+        if os.path.exists(fn):
+            with open(fn,'rb') as f:
+                contents = f.read()
+            lines_bin = contents.splitlines()
+            for ix,line_bin in enumerate( lines_bin ):
+                try:
+                    line = line_bin.decode( 'utf-8')
+                    if what in line:
+                        #print( "%s (%d): %s" % (fn, ix+1,   colorize_it(line,what)  ))
+                        print( f"{fn} ({ix+1}): {colorize_it(line,what)}" )
+                except Exception as _:
+                    #most of the time, no desire to print here. zip, binaries, etc cause issues
+                    #print( "problem decoding in '%s'" % fn )
+                    pass
+    if len(exclude_dirs)>0:
+        xdirs = [  Path(x).as_posix()+'/' for x in exclude_dirs ]
+        print( "Note: These dirs excluded from search:",', '.join(xdirs) )
 if __name__ == '__main__':
-   main( sys.argv )
+    main( sys.argv )
 
 
 
